5e57b6f22cb71c0cd034e6d3e4edc66013731d36
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-07-19 19:23:31 +0200

    Homework - FizzBuzz game

---
+++ FizzBuzz.Homework.ConsoleApp/FizzBuzzClass.cs
@@ -0,0 +1,24 @@
+﻿namespace FizzBuzz.Homework.ConsoleApp;
+
+internal class FizzBuzzClass
+{
+	public string CheckNumber(int number)
+	{
+		if ((number % 3 == 0) && (number % 5 == 0))
+		{
+			return "FizzBuzz";
+		}
+
+		if (number % 3 == 0)
+		{
+			return "Fizz";
+		}
+
+		if (number % 5 == 0)
+		{
+			return "Buzz";
+		}
+
+		return number.ToString();
+	}
+}
---
+++ FizzBuzz.Homework.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using FizzBuzz.Homework.ConsoleApp;
+
+Console.WriteLine("The program checks if number is divisible by 3, 5 or 15.");
+Console.WriteLine("Type the numbers; the letter K will end the program.");
+
+FizzBuzzClass fizzBuzz = new();
+string input;
+do
+{
+	Console.WriteLine();
+	Console.Write("Write a number: ");
+	input = Console.ReadLine().ToUpper();
+	if (int.TryParse(input, out int number))
+	{
+		Console.WriteLine($"{fizzBuzz.CheckNumber(number)}");
+	}
+} while (input != "K");
+
+Console.WriteLine("\nPress Enter to close.");
+Console.ReadLine();
