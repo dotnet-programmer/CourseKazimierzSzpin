4bfd167bf4999008c27ebda0709905a5b57983bf
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-07-19 19:09:40 +0200

    Homework - Even or odd

---
+++ EvenOrOdd.Homework.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("The program checks the parity of numbers.");
+Console.WriteLine("Type the numbers; the letter K will end the program.");
+
+string input;
+do
+{
+	Console.WriteLine();
+	Console.Write("Write a number: ");
+	input = Console.ReadLine().ToUpper();
+	if (int.TryParse(input, out int number))
+	{
+		Console.WriteLine($"Number {number} is {((number % 2 == 0) ? "even" : "odd")}.");
+	}
+} while (input != "K");
+
+Console.WriteLine("\nPress Enter to close.");
+Console.ReadLine();
