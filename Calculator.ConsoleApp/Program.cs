80007760211ab2723e2236406dce9653d1c9e25e
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-07-19 17:01:37 +0200

    Homework - WelcomeApp

--- Calculator.ConsoleApp/Program.cs
+++ Calculator.ConsoleApp/Program.cs
@@ -1,24 +1,24 @@
 ﻿try
 {
-	Console.WriteLine("Podaj 1 liczbę:");
+	Console.Write("Write 1st number: ");
 	var number1 = GetInput();
 
-	Console.WriteLine("Jaką operacje chcesz wykonać? Możliwe operacje to: '+', '-', '*', '/'.");
+	Console.Write("Write type of operation (+, -, *, /, %): ");
 	var action = Console.ReadLine();
 
-	Console.WriteLine("Podaj 2 liczbę:");
+	Console.Write("Write 2nd number: ");
 	var number2 = GetInput();
 
-	Console.WriteLine($"Wynik Twojego działania to: {Calculate(number1, number2, action)}");
+	Console.WriteLine($"\n{number1} {action} {number2} = {Calculate(number1, number2, action)}\n");
 }
 catch (Exception ex)
 {
-	//logowanie do pliku
+	//log to file
 	Console.WriteLine(ex.Message);
 }
 finally
 {
-	Console.WriteLine("\nKoniec programu.");
+	Console.WriteLine("\nPress any key to close.");
 	Console.ReadLine();
 }
 
@@ -26,7 +26,7 @@ static int GetInput()
 {
 	if (!int.TryParse(Console.ReadLine(), out int input))
 	{
-		throw new Exception("Podana wartość jest nieprawidłowa.");
+		throw new Exception("Wrong input!");
 	}
 	return input;
 }
@@ -37,5 +37,6 @@ static int Calculate(int number1, int number2, string action) => action switch
 	"-" => number1 - number2,
 	"*" => number1 * number2,
 	"/" => number1 / number2,
-	_ => throw new Exception("Wybrałeś złą operację!"),
+	"%" => number1 % number2,
+	_ => throw new Exception("Unknown operation!"),
 };
---
+++ WelcomeApp.Homework.ConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.Write("Write your name: ");
+string name = GetTextInput();
+
+Console.Write("Write year of birth: ");
+int year = GetYear();
+
+Console.Write("Write month of birth: ");
+int month = GetMonth();
+
+Console.Write("Write day of birth: ");
+int day = GetDay(year, month);
+
+Console.Write("Write city of birth: ");
+string birthplace = GetTextInput();
+
+DateTime birthDate = new(year, month, day);
+int age = (DateTime.Now.DayOfYear > birthDate.DayOfYear) ? (DateTime.Now.Year - year) : (DateTime.Now.Year - year - 1);
+
+Console.WriteLine();
+Console.WriteLine($"Hello {name}, you were born in {birthplace} and you are {age} years old.");
+Console.ReadLine();
+
+static string GetTextInput()
+{
+	string? birthplace;
+	while (string.IsNullOrWhiteSpace(birthplace = Console.ReadLine()))
+	{
+		ErrorMessage();
+	}
+	return birthplace;
+}
+
+static int GetYear()
+{
+	int year;
+	while (!int.TryParse(Console.ReadLine(), out year) || year < 1850 || year > DateTime.Now.Year)
+	{
+		ErrorMessage();
+	}
+	return year;
+}
+
+static int GetMonth()
+{
+	int month;
+	while (!int.TryParse(Console.ReadLine(), out month) || month < 1 || month > 12)
+	{
+		ErrorMessage();
+	}
+	return month;
+}
+
+static int GetDay(int year, int month)
+{
+	int day;
+	while (!int.TryParse(Console.ReadLine(), out day) || day < 1 || day > DateTime.DaysInMonth(year, month))
+	{
+		ErrorMessage();
+	}
+	return day;
+}
+
+static void ErrorMessage() => Console.Write("Wrong input!");
