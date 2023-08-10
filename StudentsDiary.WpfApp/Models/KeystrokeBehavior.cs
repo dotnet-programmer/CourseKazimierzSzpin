2229df8eda82e8fc0747ab232be801563aacbb21
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-08-10 16:28:07 +0200

    Keystroke Behavior

---
+++ StudentsDiary.WpfApp/Models/KeystrokeBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace StudentsDiary.WpfApp.Models;
+
+// INFO - MVVM Zamykanie okna 5 - użycie Keystroke Behavior
+internal class KeystrokeBehavior : Behavior<Window>
+{
+	public Key PressedKey { get; set; }
+
+	protected override void OnAttached()
+	{
+		Window window = AssociatedObject;
+		if (window != null)
+		{
+			window.KeyDown += Window_KeyDown;
+		}
+	}
+
+	private void Window_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == PressedKey)
+		{
+			(sender as Window)?.Close();
+		}
+	}
+}
