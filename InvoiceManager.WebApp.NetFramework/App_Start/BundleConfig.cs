20e2e84e4c0556fd191b041933867a2de47af3f2
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-12-05 19:39:55 +0100

    InvoiceManager - update

--- InvoiceManager.WebApp.NetFramework/App_Start/BundleConfig.cs
+++ InvoiceManager.WebApp.NetFramework/App_Start/BundleConfig.cs
@@ -8,17 +8,27 @@ namespace InvoiceManager.WebApp.NetFramework
 		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js", "~/Scripts/jquery.cookie.js"));
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+				"~/Scripts/jquery-{version}.js", 
+				"~/Scripts/jquery.cookie.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+				"~/Scripts/jquery.validate*"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+				"~/Scripts/modernizr-*"));
 
-			bundles.Add(new Bundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/bootstrap-datepicker.js"));
+			bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+				"~/Scripts/bootstrap.js", 
+				"~/Scripts/bootstrap-datepicker.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/bootstrap-datepicker.css", "~/Content/bootstrap-datepicker3.css", "~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/bootstrap.css", 
+				"~/Content/bootstrap-datepicker.css", 
+				"~/Content/bootstrap-datepicker3.css", 
+				"~/Content/site.css"));
 		}
 	}
 }
