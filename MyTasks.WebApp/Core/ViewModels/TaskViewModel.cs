3b035c6fffeb3128c112764943fba28effe68035
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-12-21 23:25:19 +0100

    MyTasks - lesson 6 - views

---
+++ MyTasks.WebApp/Core/ViewModels/TaskViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyTasks.WebApp.Core.Models.Domains;
+using Task = MyTasks.WebApp.Core.Models.Domains.Task;
+
+namespace MyTasks.WebApp.Core.ViewModels;
+
+public class TaskViewModel
+{
+    public string Heading { get; set; }
+    public Task	Task{ get; set; }
+    public IEnumerable<Category> Categories { get; set; }
+}
---
+++ MyTasks.WebApp/Core/ViewModels/TasksViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MyTasks.WebApp.Core.Models;
+using MyTasks.WebApp.Core.Models.Domains;
+using Task = MyTasks.WebApp.Core.Models.Domains.Task;
+
+namespace MyTasks.WebApp.Core.ViewModels;
+
+public class TasksViewModel
+{
+	public IEnumerable<Task> Tasks { get; set; }
+	public IEnumerable<Category> Categories { get; set; }
+	public FilterTasks FilterTasks { get; set; }
+}
