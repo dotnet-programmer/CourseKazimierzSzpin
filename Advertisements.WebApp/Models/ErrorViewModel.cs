2394e114b4105031980ac6e51d78eec911bcdd83
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-05 19:22:44 +0100

    New project - Advertisements.WebApp

---
+++ Advertisements.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Advertisements.WebApp.Models;
+
+public class ErrorViewModel
+{
+	public string? RequestId { get; set; }
+
+	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
