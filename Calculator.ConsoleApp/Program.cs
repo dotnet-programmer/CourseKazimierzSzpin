10154166060cc86d619e5e00455ff289487ff0e6
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-06-07 14:58:55 +0200

    Refactoring - Calculator

--- Calculator.ConsoleApp/Program.cs
+++ Calculator.ConsoleApp/Program.cs
@@ -1,13 +1,13 @@
 ﻿try
 {
 	Console.Write("Write 1st number: ");
-	var number1 = GetInput();
+	int number1 = GetInput();
 
 	Console.Write("Write type of operation (+, -, *, /, %): ");
-	var action = Console.ReadLine();
+	string action = Console.ReadLine();
 
 	Console.Write("Write 2nd number: ");
-	var number2 = GetInput();
+	int number2 = GetInput();
 
 	Console.WriteLine($"\n{number1} {action} {number2} = {Calculate(number1, number2, action)}\n");
 }
@@ -31,12 +31,13 @@ static int GetInput()
 	return input;
 }
 
-static int Calculate(int number1, int number2, string action) => action switch
-{
-	"+" => number1 + number2,
-	"-" => number1 - number2,
-	"*" => number1 * number2,
-	"/" => number1 / number2,
-	"%" => number1 % number2,
-	_ => throw new Exception("Unknown operation!"),
-};
+static int Calculate(int number1, int number2, string action)
+	=> action switch
+	{
+		"+" => number1 + number2,
+		"-" => number1 - number2,
+		"*" => number1 * number2,
+		"/" => number1 / number2,
+		"%" => number1 % number2,
+		_ => throw new Exception("Unknown operation!"),
+	};
