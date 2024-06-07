3e0d0bcc15237ce2152a9d7175437d05848ae32f
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-06-07 14:53:32 +0200

    String encryption update

---
+++ TextEncryption.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using TextEncryption.Lib;
+
+string password = "hasło do zaszyfrowania";
+StringCipher stringCipher = new("2");
+string encryptedPassword = stringCipher.Encrypt(password);
+string decryptedPassword = stringCipher.Decrypt(encryptedPassword);
+
+Console.WriteLine($"password = {password}");
+Console.WriteLine($"encryptedPassword = {encryptedPassword}");
+Console.WriteLine($"decryptedPassword = {decryptedPassword}");
+
+Console.ReadLine();
--- TextEncryption.Lib/StringCipher.cs
+++ TextEncryption.Lib/StringCipher.cs
@@ -3,30 +3,30 @@ using System.Text;
 
 namespace TextEncryption.Lib;
 
-public class StringCipher
+public class StringCipher(string key)
 {
-	// This constant is used to determine the keysize of the encryption algorithm in bits.
+	// This constant is used to determine the key size of the encryption algorithm in bits.
 	// We divide this by 8 within the code below to get the equivalent number of bytes.
-	private const int Keysize = 128;
+	private const int KeySize = 128;
 
 	// This constant determines the number of iterations for the password bytes generation function.
 	private const int DerivationIterations = 1000;
 
-	private readonly string _key;
-
-	public StringCipher(string key) => _key = key;
+	private readonly string _key = key;
 
 	public string Encrypt(string plainText)
 	{
-		// Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+		// Salt and IV is randomly generated each time, but is pre-prepared to encrypted cipher text
 		// so that the same Salt and IV values can be used when decrypting.
 		var saltStringBytes = Generate128BitsOfRandomEntropy();
 		var ivStringBytes = Generate128BitsOfRandomEntropy();
 		var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-		using (var password = new Rfc2898DeriveBytes(_key, saltStringBytes, DerivationIterations))
+
+		using (var password = new Rfc2898DeriveBytes(_key, saltStringBytes, DerivationIterations, HashAlgorithmName.SHA1))
 		{
-			var keyBytes = password.GetBytes(Keysize / 8);
-			using (var symmetricKey = new RijndaelManaged())
+			var keyBytes = password.GetBytes(KeySize / 8);
+
+			using (var symmetricKey = Aes.Create())
 			{
 				symmetricKey.BlockSize = 128;
 				symmetricKey.Mode = CipherMode.CBC;
@@ -39,6 +39,7 @@ public class StringCipher
 						{
 							cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
 							cryptoStream.FlushFinalBlock();
+
 							// Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
 							var cipherTextBytes = saltStringBytes;
 							cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
@@ -58,17 +59,21 @@ public class StringCipher
 		// Get the complete stream of bytes that represent:
 		// [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
 		var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
-		// Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
-		var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
-		// Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
-		var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
-		// Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
-		var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
 
-		using (var password = new Rfc2898DeriveBytes(_key, saltStringBytes, DerivationIterations))
+		// Get the saltBytes by extracting the first 32 bytes from the supplied cipherText bytes.
+		var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(KeySize / 8).ToArray();
+
+		// Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
+		var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(KeySize / 8).Take(KeySize / 8).ToArray();
+
+		// Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
+		var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((KeySize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((KeySize / 8) * 2)).ToArray();
+
+		using (var password = new Rfc2898DeriveBytes(_key, saltStringBytes, DerivationIterations, HashAlgorithmName.SHA1))
 		{
-			var keyBytes = password.GetBytes(Keysize / 8);
-			using (var symmetricKey = new RijndaelManaged())
+			var keyBytes = password.GetBytes(KeySize / 8);
+
+			using (var symmetricKey = Aes.Create())
 			{
 				symmetricKey.BlockSize = 128;
 				symmetricKey.Mode = CipherMode.CBC;
@@ -90,108 +95,14 @@ public class StringCipher
 
 	private static byte[] Generate128BitsOfRandomEntropy()
 	{
-		// 32 Bytes will give us 256 bits.
+		// 16 Bytes will give us 128 bits.
 		var randomBytes = new byte[16];
 
-		// Fill the array with cryptographically secure random bytes.
-		using (var rngCsp = new RNGCryptoServiceProvider())
+		using (var randomNumberGenerator = RandomNumberGenerator.Create())
 		{
-			rngCsp.GetBytes(randomBytes);
+			randomNumberGenerator.GetBytes(randomBytes);
 		}
 
 		return randomBytes;
 	}
 }
-
-//public static class StringCipher
-//{
-//	// This constant is used to determine the keysize of the encryption algorithm in bits.
-//	// We divide this by 8 within the code below to get the equivalent number of bytes.
-//	private const int Keysize = 256;
-
-//	// This constant determines the number of iterations for the password bytes generation function.
-//	private const int DerivationIterations = 1000;
-
-//	public static string Encrypt(string plainText, string passPhrase)
-//	{
-//		// Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
-//		// so that the same Salt and IV values can be used when decrypting.
-//		var saltStringBytes = Generate256BitsOfRandomEntropy();
-//		var ivStringBytes = Generate256BitsOfRandomEntropy();
-//		var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-//		using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
-//		{
-//			var keyBytes = password.GetBytes(Keysize / 8);
-//			using (var symmetricKey = new RijndaelManaged())
-//			{
-//				symmetricKey.BlockSize = 256;
-//				symmetricKey.Mode = CipherMode.CBC;
-//				symmetricKey.Padding = PaddingMode.PKCS7;
-//				using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
-//				{
-//					using (var memoryStream = new MemoryStream())
-//					{
-//						using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-//						{
-//							cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-//							cryptoStream.FlushFinalBlock();
-//							// Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
-//							var cipherTextBytes = saltStringBytes;
-//							cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-//							cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-//							memoryStream.Close();
-//							cryptoStream.Close();
-//							return Convert.ToBase64String(cipherTextBytes);
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
-
-//	public static string Decrypt(string cipherText, string passPhrase)
-//	{
-//		// Get the complete stream of bytes that represent:
-//		// [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
-//		var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
-//		// Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
-//		var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
-//		// Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
-//		var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
-//		// Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
-//		var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
-
-//		using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
-//		{
-//			var keyBytes = password.GetBytes(Keysize / 8);
-//			using (var symmetricKey = new RijndaelManaged())
-//			{
-//				symmetricKey.BlockSize = 256;
-//				symmetricKey.Mode = CipherMode.CBC;
-//				symmetricKey.Padding = PaddingMode.PKCS7;
-//				using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
-//				{
-//					using (var memoryStream = new MemoryStream(cipherTextBytes))
-//					{
-//						using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-//						using (var streamReader = new StreamReader(cryptoStream, Encoding.UTF8))
-//						{
-//							return streamReader.ReadToEnd();
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
-
-//	private static byte[] Generate256BitsOfRandomEntropy()
-//	{
-//		var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
-//		using (var rngCsp = new RNGCryptoServiceProvider())
-//		{
-//			// Fill the array with cryptographically secure random bytes.
-//			rngCsp.GetBytes(randomBytes);
-//		}
-//		return randomBytes;
-//	}
-//}
